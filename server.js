6b7442894073c07ccc4ef9c07ae8c69be19ff6f4
Louis Yacksmith <louisyscarleton@gmail.com>
2020-11-19 20:23:43 -0500

    server handlebar ready for customization

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const PORT = process.env.PORT || 3000
 
@@ -7,6 +8,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(require('./routes/html-routes.js'))
+
 app.listen(PORT, function () {
     console.log('Listening on port: ' + PORT)
-  })
+})
