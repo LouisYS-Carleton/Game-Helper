c85ef8a0afb0d0c6868f95355519becf7ed9987c
Louis Yacksmith <louisyscarleton@gmail.com>
2020-11-20 21:54:32 -0500

    initial navbar

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(require('./routes/html-routes.js'))
 
 app.listen(PORT, function () {
-    console.log('Listening on port: ' + PORT)
+  console.log('Listening on port: ' + PORT)
 })
