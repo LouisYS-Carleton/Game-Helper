1b17d70fd75045e7e9e282f195358e673f4b1dae
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-22 01:46:30 -0500

    Modify homeroute to retrieve api data and mark owned games

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,34 @@
 const router = require('express').Router()
+const { getCurrentGames, getUpcomingGames } = require('./helper/gamespot.js')
+const db = require('../models')
 
-router.get('/', function(req, res) {
-  res.render('home');
-});
+router.get('/', async function (req, res) {
+  try {
+    const results = await Promise.all([
+      getCurrentGames(),
+      getUpcomingGames(),
+      db.Game.findAll({
+        include: db.Image,
+      }),
+    ])
+    const [currentGames, upcomingGames, ownedGames] = results
+    markOwned(currentGames, ownedGames)
+    console.log(upcomingGames)
+    res.status(200).render('home')
+  } catch (err) {
+    console.log(err)
+    res.status(401).json({ error: err })
+  }
+})
+
+function markOwned(currentGames, ownedGames) {
+  for (const game of currentGames) {
+    if (ownedGames.find((data) => game.apiId === data.apiId)) {
+      game.owned = true
+    } else {
+      game.owned = false
+    }
+  }
+}
 
 module.exports = router
