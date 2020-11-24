5250896a421dce35639e777d9b72b8d3f26a0a58
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-24 12:00:03 -0500

    Require searchGames function in routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
-const { getCurrentGames, getUpcomingGames } = require('./helper/gamespot.js')
+const {
+  getCurrentGames,
+  getUpcomingGames,
+  searchGames,
+} = require('./helper/gamespot.js')
 const db = require('../models')
 
 router.get('/', async function (req, res) {
