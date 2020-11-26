09f866363fef111f9a869308ff968a1984697955
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-26 12:42:53 -0500

    Display user email on dashboard

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,6 +22,7 @@ router.get('/', isAuthenticate, async function (req, res) {
       currentGames,
       upcomingGames,
       ownedGames: formatOwnedGames(ownedGames),
+      userEmail: req.user.email,
     })
   } catch (err) {
     console.log(err)
